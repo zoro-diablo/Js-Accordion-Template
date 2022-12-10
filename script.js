31fa29ab226a1a8422743960d49dded77253a8fa
zorodiablo <vyshnavk117@gmail.com>
2022-12-10 10:09:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const accordion=document.querySelectorAll('.content-container');
+
+for(let i=0;i<accordion.length;i++){
+
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    })
+};
+
